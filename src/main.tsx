181181a7b00cb0ed49cf66e49ab6a2562416d64f
danisyam <danisyamsudin0@gmail.com>
2024-09-08 00:54:26 +0700

    has route f

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
