eafd4434aa4d281030826283fda5e587e72c5781
danisyam <danisyamsudin0@gmail.com>
2024-09-08 05:53:45 +0700

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({	
-	site: 'https://danisyam.github.io',
-	integrations: [mdx(), sitemap()],
+    site: 'https://danisyam.github.io',
+    integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
