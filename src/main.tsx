05b06d5d07822d6d1b8a9a2c1de9b1190f9f6d50
danisyam <danisyamsudin0@gmail.com>
2024-09-07 23:03:32 +0700

    Fix routing issue for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Home from '@/pages/Home';
 import About from '@/pages/About';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
